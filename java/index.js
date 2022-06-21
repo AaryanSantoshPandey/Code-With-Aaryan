9ccc507c3b5604414eb9a5ae0227703848a0a2d3
Code With Aaryan <85786184+aaryanpandey353@users.noreply.github.com>
2022-06-21 17:37:38 +0530

    Add files via upload

---
+++ java/index.js
@@ -0,0 +1,21 @@
+console.log("Script is ruinning...")
+function initThemeSelector() {
+    const themeSelect = document.getElementById("themeSelect");
+    const themeStylesheetLink = document.getElementById("themeStylesheetLink");
+    const currentTheme = localStorage.getItem("theme") || "default";
+
+    function activateTheme(themeName) {
+        themeStylesheetLink.setAttribute("href", `css/themes/${themeName}.css`);
+    }
+
+    themeSelect.addEventListener("change", () => {
+        activateTheme(themeSelect.value);
+        localStorage.setItem("theme", themeSelect.value);
+    });
+
+
+    themeSelect.value = currentTheme;
+    activateTheme(currentTheme);
+}
+
+initThemeSelector();
