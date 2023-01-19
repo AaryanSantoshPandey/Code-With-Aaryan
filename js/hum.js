17851ccd86bc0718fac54ddcaf3d928612dce103
Code With Aaryan <85786184+aaryanpandey353@users.noreply.github.com>
2023-01-19 17:12:51 +0530

    Delete js directory

--- js/hum.js
+++
@@ -1,6 +0,0 @@
-console.log("Script running...")
-
-// document.querySelector('.cross').style.display = 'none';
-document.querySelector('.humm').addEventListener("click", ()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebarGo'); 
-})
